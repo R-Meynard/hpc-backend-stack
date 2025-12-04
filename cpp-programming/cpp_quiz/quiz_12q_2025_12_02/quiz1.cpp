60ff3954e0b5318069f730cf27447d88cf431454
R-Meynard <meynard.rolih@gmail.com>
2025-12-04 13:23:02 +0000

    feat(cpp-quizz): add 2/12-question quiz from Dec 2, 2025

---
+++ cpp-programming/cpp_quiz/quiz_12q_2025_12_02/quiz1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main(int argc, char *argv[]){
+
+    // Question 1 : 
+    int a = 5;
+    std::cout << " " << ++a << std::endl; // What is the output ?
+    // Answer : 6
+
+    // Question 2 :
+    // On peut assigner un int * à un const int*
+    int testValue = 20;
+    int* value = &testValue;
+    const int* value2 = value;
+    return 0;
+}
