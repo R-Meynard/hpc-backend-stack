e9b4f4900317c82b43c44633f459c39e3956a869
R-Meynard <meynard.rolih@gmail.com>
2026-01-15 15:29:30 +0000

    feat(cpp-exercises): Add W3Schools OOP exercises on polymorphism

---
+++ cpp-programming/00_basics/w3schools_cpp_exercises/cpp_polymorphism/exercises_cpp_polymorphism.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+
+    // Question 1 : What does polymorphism mean in C++ ?
+    // Answer :
+    // Many forms
+
+    // Question 2 : What is the main purpose of polymorphism ?
+    // Answer :
+    // To perform a single action in different ways
+
+    // Question 3 : True or False:
+    // Polymorphism allows derived classes to provide their own implementation of base class methods.
+    // Answer :
+    // True
+
+    // Question 3 : Why is polymorphism useful in programming ?
+    // Answer :
+    // It allows for code reusability and flexibility in how methods are implemented in derived classes
+    return 0;
+}
