564c7edbef65c2ea7949ffcfdbad7ac204300807
R-Meynard <meynard.rolih@gmail.com>
2026-02-22 20:30:43 +0000

    chore(stack): add stack_only stack allocation example
    Add stack_only(b :i32) in src/the_stack.rs to demonstrate a local stack variable and print b and c.

--- rust-programming/udemy/learn_Rust_by_Building_Real_Applications/example/src/main.rs
+++ rust-programming/udemy/learn_Rust_by_Building_Real_Applications/example/src/main.rs
@@ -1,3 +1,7 @@
+mod the_stack;
+
 fn main() {
     println!("Hello, world!");
+    let a: i32 = 2;
+    the_stack::stack_only(a);
 }
---
+++ rust-programming/udemy/learn_Rust_by_Building_Real_Applications/example/src/the_stack.rs
@@ -0,0 +1,4 @@
+pub fn stack_only(b: i32) {
+    let c: i32 = 3;
+    println!("b = {}, c = {}", b, c)
+}
