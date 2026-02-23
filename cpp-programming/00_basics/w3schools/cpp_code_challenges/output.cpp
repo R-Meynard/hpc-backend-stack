ed6f1225d7ee38d8e639e2859556d768796d3f0c
R-Meynard <meynard.rolih@gmail.com>
2026-02-23 12:11:16 +0000

    Add W3Schools C++ code challenges
    - Add output challenge
    - Add syntax challenge

---
+++ cpp-programming/00_basics/w3schools/cpp_code_challenges/output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    // 1) Print your name witha new line
+    std::cout<< "Rolih \n" << std::endl;
+    // 2) Print your age
+    std::cout << "I'm 35 years old.";
+    return 0;
+}
---
+++ cpp-programming/00_basics/w3schools/cpp_code_challenges/syntax.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// 1) Write the main() function here
+int main(){
+    // 2) Use cout to print "Hello, C++!"
+    std::cout << "Hello, C++!"<< std::endl;
+    // 3) End the program with return 0;
+    return 0;
+}
